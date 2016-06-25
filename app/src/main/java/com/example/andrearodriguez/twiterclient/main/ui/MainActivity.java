9e46b8190f4181cb4ed790d5a7553fdb1c6326e2
liyi85 <liyi85@gmail.com>
2016-06-25 15:25:41 -0500

    creacion de las secciones, titulos e imagenes, creacion e implementacion del adapter para los tabs

--- app/src/main/java/com/example/andrearodriguez/twiterclient/main/ui/MainActivity.java
+++ app/src/main/java/com/example/andrearodriguez/twiterclient/main/ui/MainActivity.java
@@ -3,14 +3,20 @@ package com.example.andrearodriguez.twiterclient.main.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.andrearodriguez.twiterclient.LoginActivity;
 import com.example.andrearodriguez.twiterclient.R;
+import com.example.andrearodriguez.twiterclient.images.HashtagsFragment;
+import com.example.andrearodriguez.twiterclient.images.ImageFragment;
+import com.example.andrearodriguez.twiterclient.main.adapter.MainSectionsPagerAdapter;
 import com.twitter.sdk.android.Twitter;
 
 import butterknife.BindView;
@@ -23,19 +29,36 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.appbar)
     AppBarLayout appbar;
     @BindView(R.id.container)
-    ViewPager container;
+    ViewPager viewPager;
+    @BindView(R.id.tabs)
+    TabLayout tabs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
         setSupportActionBar(toolbar);
+        setupAdapter();
     }
+
+    private void setupAdapter() {
+        Fragment[] fragments = new Fragment[]{new ImageFragment(), new HashtagsFragment()};
+        String[] titles = new String[]{getString(R.string.main_header_images), getString(R.string.main_header_hastags)};
+        MainSectionsPagerAdapter adapter = new MainSectionsPagerAdapter(getSupportFragmentManager(), titles, fragments);
+
+        viewPager.setAdapter(adapter);
+        tabs.setupWithViewPager(viewPager);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.i("ABC", "onCreateOptionsMenu");
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return super.onCreateOptionsMenu(menu);
     }
 
